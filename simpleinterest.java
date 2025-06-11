8315244f7c521829f60750c2a6dbc667b3a5b160
Nagbhushan244 <01fe23bca244@kletech.ac.in>
2025-06-11 08:53:23 +0530

    Save it

--- simpleinterest.java
+++ simpleinterest.java
@@ -3,5 +3,6 @@ class simpleinterest{
         double p=10000,r=2,t=2,si;
         si=(p*r*t)/100;
         System.out.println("Simple Interest = "+si);
+        System.out.println("Added to main");
     }
 }
