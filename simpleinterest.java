95186cd40c8af8cd6138db1329519d236ae3fc3c
Nagbhushan244 <01fe23bca244@kletech.ac.in>
2025-06-11 08:29:52 +0530

    Simple Interest

---
+++ simpleinterest.java
@@ -0,0 +1,7 @@
+class simpleinterest{
+    public static void main(String[] args) {
+        double p=10000,r=2,t=2,si;
+        si=(p*r*t)/100;
+        System.out.println("Simple Interest = "+si);
+    }
+}
